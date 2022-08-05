3e471e27a640780d9ee2e5075f176a1836077e1d
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-08-05 10:15:36 +0300

    updated cors

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ const app = express();
 
 // set up session cookies
 app.use(
-  cors({
-    origin: "https://www.userben.ch",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  }),
+  cors(
+  //   {
+  //   origin: "https://www.userben.ch",
+  //   methods: "GET,POST,PUT,DELETE",
+  //   credentials: true,
+  // }
+  ),
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: ["ilikecookies"],
