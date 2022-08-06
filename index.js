10d3d0a4bb3e3a808293a13e064e2ddb84270850
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-08-06 13:30:41 +0300

    updated v9

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ const jwt = require("jsonwebtoken");
 const authRoute = require("./routes/auth");
 const cors = require("cors");
 const app = express();
+const corsFix = require("./corsFix")
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 // set up session cookies
+app.use(corsFix)
 app.use(
   cors(
     {
