9ae75ba3d6c455e010c0c262e020a3731aefd795
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-08-15 18:15:10 +0300

    updated v5

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   }),
   (req,res,next) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', 'https://userben.ch')
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
